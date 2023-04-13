6873e83c4008df2e0646935a2b6f7dd965194c3a
chomp <felipesalazar@vtr.net>
2023-04-13 11:18:17 -0400

    Ayudantía 2

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,7 @@ public class Movement : MonoBehaviour
     [SerializeField] private GameObject player;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+    private Animator anim;
 
     [Header("Movimiento")]
     public float characterSpeed;
@@ -26,6 +27,7 @@ public class Movement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -38,14 +40,20 @@ public class Movement : MonoBehaviour
     void MovementUpdate()
     {
         rb.velocity = new Vector2(characterSpeed * Input.GetAxisRaw("Horizontal"), rb.velocity.y);
-        if(rb.velocity.x < 0)
+        if(rb.velocity.x > 0){
+            anim.SetBool("isMoving", true);
+        }
+        else{
+            anim.SetBool("isMoving", false);
+        }
+        /*if(rb.velocity.x < 0)
         {
             sr.flipX = true;
         }
         else if(rb.velocity.x > 0)
         {
             sr.flipX = false;
-        }
+        }*/
     }
 
     void JumpingAndFalling()
@@ -54,10 +62,12 @@ public class Movement : MonoBehaviour
         if(isGrounded)
         {
             canDoubleJump = true;
+            anim.SetBool("isJumping", false);
         }
         if(Input.GetButtonDown("Jump")){
             if(isGrounded || canDoubleJump)
             {
+                anim.SetBool("isJumping", true);
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
                 if(!isGrounded && canDoubleJump)
                 {
